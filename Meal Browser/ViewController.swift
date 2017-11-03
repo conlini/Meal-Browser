545667b23acc738ad8503c3a811968b083655b41
Aditya Bhasin <adbhasin@cisco.com>
2017-11-03 11:28:34 +0530

    Initial Commit

---
+++ Meal Browser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meal Browser
+//
+//  Created by adbhasin on 03/11/17.
+//  Copyright © 2017 conlini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
