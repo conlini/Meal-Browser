2bc76586cd9f653a4d4934db3de93242b08be773
conlini <conlini@gmail.com>
2017-11-03 22:23:43 +0530

    customizing view controllers

--- Meal Browser/ViewController.swift
+++ Meal Browser/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    // MARK: Properties
+    @IBOutlet weak var mealLabel: UILabel!
+    @IBOutlet weak var mealNameTextFld: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mealNameTextFld.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealLabel.text = textField.text
+    }
+    
+    // MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected dictionary with images but was - :\(info)")
+        }
+        photoImageView.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: UINavigationControllerDelegate
 
-
+    // MARK: Actions
+  
+    @IBAction func selectImageFromPhotoLib(_ sender: UITapGestureRecognizer) {
+        mealNameTextFld.resignFirstResponder()
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func setDefaultLabel(_ sender: UIButton) {
+        mealLabel.text = "Meal Name"
+        mealNameTextFld.text = nil
+    }
+    
 }
 
